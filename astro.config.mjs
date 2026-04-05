273cf33ebdc4b015744f0811946336d26e75d3b9
David Berlin <47757213+davbree@users.noreply.github.com>
2026-04-05 12:38:56 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      allowedHosts: ['devserver-main--ds-astro-platform-starter.netlifystg.app'],
     },
-    integrations: [react()],
-    adapter: netlify()
+  },
+  integrations: [react()],
+  adapter: netlify(),
 });
